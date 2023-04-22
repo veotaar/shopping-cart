3229818f06729c7f4088691ea24af18790e76222
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-22 22:38:56 +0300

    Add global state provider to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch';
+import { CartProvider } from './state/CartProvider';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouteSwitch />
+    <CartProvider>
+      <RouteSwitch />
+    </CartProvider>
   </React.StrictMode>
 );
