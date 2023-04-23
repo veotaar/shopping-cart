15c723f59bab03d1c8518d37cb2394be8ba9afda
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-23 22:58:21 +0300

    Remove unnecessary buttons from Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,3 @@
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-import cn from 'classnames';
 import { useCartDispatch } from '../state/CartProvider';
 
 function Item({ item }) {
@@ -13,47 +11,20 @@ function Item({ item }) {
         </p>
       </div>
       <div className="w-2/6 text-gray-800 sm:w-1/6">€{item.price.toFixed(2)}</div>
-      <div className="flex w-2/6 items-center justify-center gap-1">
+      <div>
         <button
-          className={cn(
-            { hidden: item.amountInCart < 1 },
-            'rounded-full bg-green-700  px-2 py-2 text-xl text-white hover:bg-green-900'
-          )}
+          className="truncate rounded bg-green-200 px-2 py-2 text-green-900 transition-colors hover:bg-green-900 hover:text-white active:bg-green-700"
           type="button"
           onClick={() =>
             dispatch({
-              type: 'update',
+              type: 'increase',
               payload: {
                 ...item,
-                amountInCart: item.amountInCart - 1,
               },
             })
           }
         >
-          <AiOutlineMinus />
-        </button>
-        <div
-          className={cn(
-            { hidden: item.amountInCart < 1 },
-            'w-8 rounded-full border border-gray-300 bg-gray-100 p-1 text-center sm:w-12'
-          )}
-        >
-          {item.amountInCart}
-        </div>
-        <button
-          className="rounded-full bg-green-700 px-2 py-2 text-xl text-white hover:bg-green-900"
-          type="button"
-          onClick={() =>
-            dispatch({
-              type: 'update',
-              payload: {
-                ...item,
-                amountInCart: item.amountInCart + 1,
-              },
-            })
-          }
-        >
-          <AiOutlinePlus />
+          Add to Cart
         </button>
       </div>
     </div>
