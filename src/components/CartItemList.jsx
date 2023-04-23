0b5ea22be026272a3f6adac243680e83d8e61cf0
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-23 20:48:49 +0300

    Add ability to clear cart

--- src/components/CartItemList.jsx
+++ src/components/CartItemList.jsx
@@ -1,14 +1,24 @@
 import CartItem from './CartItem';
-import { useItemsInCart } from '../state/CartProvider';
+import { useItemsInCart, useCartDispatch } from '../state/CartProvider';
 
 function CartItemList() {
   const items = useItemsInCart();
+  const dispatch = useCartDispatch();
 
   return (
-    <div>
-      {items.map((item) => (
-        <CartItem key={item.id} item={item} />
-      ))}
+    <div className="flex max-w-2xl flex-col">
+      <div className="flex  flex-col gap-2 border-y-2 border-y-gray-300 py-2">
+        {items.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+      <button
+        className="self-start text-gray-600 hover:font-bold hover:text-red-700"
+        onClick={() => dispatch({ type: 'reset' })}
+        type="button"
+      >
+        Clear Cart
+      </button>
     </div>
   );
 }
