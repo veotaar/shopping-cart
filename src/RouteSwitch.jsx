6406a9b32d45b82693e5e91ce66afda17e85547f
Yalçıncan Ulus <41164799+veotaar@users.noreply.github.com>
2023-04-26 14:37:44 +0300

    Switch to using hash router for gh-pages

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './pages/Cart';
 import Shop from './pages/Shop';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 const RouteSwitch = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/cart" element={<Cart />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default RouteSwitch;
